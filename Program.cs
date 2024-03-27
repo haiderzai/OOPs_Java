c7744cf919421992287ee7dde78c42f230d03d11
haiderzai <haiderzai@gmail.com>
2024-03-27 05:26:26 +0100

    inheritance This is a new chagnes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Inheritance
+{
+    class Animal
+    {
+        public string name;
+        public void display()
+        {
+            Console.WriteLine("I am an animal");
+
+        }
+    }
+    // derived class
+    class Dog: Animal
+    {
+        public void getName()
+        {
+            Console.WriteLine("My name is" + name);
+        }
+
+
+    }
+
+    // class to main menue
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog laborder = new Dog();
+            laborder.name="Tazi dog";
+            laborder.display();
+            laborder.getName();
+            Console.ReadLine();
+        }
+    }
+}
+
